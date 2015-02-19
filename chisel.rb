a3d71ead2c9ab4e522bda133cf9b5a3351629475
Kenny <chenkenn@gmail.com>
2015-02-18 21:56:11 -0700

    Split and starting to print # words

--- chisel.rb
+++ chisel.rb
@@ -2,34 +2,38 @@ class Chisel
 
 
   def initialize
-  @document = '# My Life in Desserts
-
-  ## Chapter 1: The Beginning
-
-  "You just *have* to try the cheesecake," he said. "Ever since it appeared in
-  **Food & Wine** this place has been packed every night."'
 
   end
-def parse(document)
-  if document[0..1] == "##"
-    print "<h2>#{document[2..-1]}</h2>"
-  elsif document[0] == "#"
-    print "<h1>#{document[1..-1]}</h1>"
-  else document[0] == "\" "
-    print "<p>\n#{document[1..-2]}\n</p>"
+  def parse(document)
+    document = document.split(" ")
+
+   document.map do |item|
+
+      if item == "##"
+        print "<h2>#{document[6..9].join(" ")}</h2>"
+      elsif item == "#"
+        print "<h1>#{document[1..4].join(" ")}</h1>"
+      else
+      end
+    end
+  #   # elsif
+  #   # else document[0] == "\" "
+  #   #   print "<p>\n#{document[1..-2]}\n</p>"
+  #   # end
   end
 
 
+  def doc
+    print @document.split.(" ")
+  end
 end
+document = '# My Life in Desserts
 
+## Chapter 1: The Beginning
 
-
-
-
-
-
-end
-
+"You just *have* to try the cheesecake," he said. "Ever since it appeared in
+**Food & Wine** this place has been packed every night."'
 
 parser = Chisel.new
-parser.parse("# My Life in Desserts")
+print parser.parse(document)
+# parser.parse("# My Life in Desserts")
