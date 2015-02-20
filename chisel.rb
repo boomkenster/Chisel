8b9c4715426a920125527f0b25cadf4d3048f3ae
Kenny <chenkenn@gmail.com>
2015-02-20 09:53:54 -0700

    updated paragraph tag

--- chisel.rb
+++ chisel.rb
@@ -20,7 +20,7 @@ class Chisel
     when line.start_with?("#")
       format_tag(line, "h1")
     else
-      format_tag(line, "p")
+      format_paragraph_tag(line, "p")
         # line = line.split(" ")
         # line.map do |words|
         # format_paragraph_line(words)
@@ -50,7 +50,12 @@ class Chisel
 
   def format_tag(line, tag)
     line.delete!("#")
-    "<#{tag}>#{line}</#{tag}>"
+    "<#{tag}>#{line}</#{tag}>\n"
+  end
+
+  def format_paragraph_tag(line, tag)
+    line.delete!("#")
+    "<#{tag}>\n#{line}\n</#{tag}>\n"
   end
 end
 document = '# My Life in Desserts
@@ -58,9 +63,9 @@ document = '# My Life in Desserts
 ## Chapter 1: The Beginning
 
 "You just *have* to try the cheesecake," he said. "Ever since it appeared in
-**Food & Wine** this place has been packed every night.
+**Food & Wine** this place has been packed every night."
 
-Lets see if this is a new p"'
+New line, new paragraph'
 
 parser = Chisel.new
 puts parser.parse(document)
