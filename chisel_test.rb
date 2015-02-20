a815c31cb664ae02153a25652f1c2dedcb05681a
Kenny <chenkenn@gmail.com>
2015-02-20 09:47:40 -0700

    updated test file

--- chisel_test.rb
+++ chisel_test.rb
@@ -31,19 +31,30 @@ class ChiselTest < Minitest::Test
 
   def test_h4_format_works
     parser = Chisel.new
-    assert_equal = "<h4> hi </h4>", parser.parse("####hi")
+    assert_equal = "<h4> hi </h4>", parser.format_line("####hi")
   end
   def test_h3_format_works
     parser = Chisel.new
-    assert_equal = "<h3> hi </h3>", parser.parse("###hi")
+    assert_equal = "<h3> hi </h3>", parser.format_line("###hi")
   end
   def test_h3_format_works
     parser = Chisel.new
-    assert_equal = "<h2> hi </h2>", parser.parse("##hi")
+    assert_equal = "<h2> hi </h2>", parser.format_line("##hi")
   end
   def test_h3_format_works
     parser = Chisel.new
-    assert_equal = "<h1> hi </h1>", parser.parse("#hi")
+    assert_equal = "<h1> hi </h1>", parser.format_line("#hi")
+  end
+
+  def test_lines_start_without_text_will_be_paragraph
+    parser = Chisel.new
+    assert_equal = "<p> hi </p>", parser.parse("hi")
+  end
+
+  def test_italic_tag_in_paragraph
+    skip
+    parser = Chisel.new
+    assert_equal "<p> hi <em> bye </em> </p>", parser.parse("hi *bye*")
   end
 
 end
